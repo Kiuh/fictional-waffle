df9b5a39f2a331d4c2fd7931cc6765ed1144447f
mertwole <mertwole@gmail.com>
2023-10-28 16:30:57 -0700

    remove secrets.json

---
+++ RoomManager/RoomManager/NetworkClient.cs
@@ -0,0 +1,21 @@
+﻿using RoomManagerApi;
+
+namespace RoomManager
+{
+	internal static class NetworkClient
+	{
+		private static HttpClient client;
+
+		static NetworkClient()
+		{
+			client = new HttpClient();
+			client.BaseAddress = new Uri("http://google.com");
+		}
+
+		public static RoomPartialInfoDto GetRoomPartialInfo(Uri room)
+		{
+			var uri = new Uri(room, "Info");
+			var res = client.GetAsync(uri).Result;
+		}
+	}
+}

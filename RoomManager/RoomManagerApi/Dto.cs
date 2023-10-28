d27c8294f46ed3e76ab870870191d5cc6100ab20
mertwole <mertwole@gmail.com>
2023-10-28 16:13:22 -0700

    RoomManager template

---
+++ RoomManager/RoomManagerApi/Dto.cs
@@ -0,0 +1,30 @@
+﻿namespace RoomManagerApi
+{
+	public class RoomPartialInfoDto
+	{
+		public required string Name { get; set; }
+		public required int ActiveUsers { get; set; }
+		public required int Capacity { get; set; }
+	}
+
+    public class RoomInfoDto
+	{
+		public required string Name { get; set; }
+		public required string ContainerName { get; set; }
+		public required DateTime DeployedAt { get; set; }
+		public required int ActiveUsers { get; set; }
+		public required int Capacity { get; set; }
+	}
+
+	public class RoomDeployRequestDto
+	{
+		public required string Name { get; set; }
+		public required string ContainerName { get; set; }
+		public required int Capacity { get; set; }
+    }
+
+	public class RoomDropRequestDto
+	{
+		public required string Name { get; set; }
+	}
+}
